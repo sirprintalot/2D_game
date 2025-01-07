9c7a34208ec64c32fc50006eb4c13f7aae6789e1
Mauricio Cardona <maucaralar@gmail.com>
2025-01-07 01:03:56 -0400

    Implemented new map transition

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -4,35 +4,45 @@ package main;
 public class EventHandler {
 
     GamePanel gp;
-    EventRect[][] eventRect;
+    EventRect[][][] eventRect;
 
     int previousEventX, previousEventY;
     boolean canTouchEvent = true;
 
 
+    //TODO check this method
+
     public EventHandler(GamePanel gp) {
 
         this.gp = gp;
-        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+        eventRect = new EventRect[gp.maxMap][gp.maxWorldCol][gp.maxWorldRow];
 
+        int map = 0;
         int row = 0;
         int col = 0;
 
-        while (row < gp.maxWorldRow && col < gp.maxWorldCol) {
+        while (map < gp.maxMap && row < gp.maxWorldRow && col < gp.maxWorldCol) {
 
-                eventRect[col][row] = new EventRect();
-                eventRect[col][row].x = 23;
-                eventRect[col][row].y = 23;
-                eventRect[col][row].width = 2;
-                eventRect[col][row].height = 2;
-                eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
-                eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+                eventRect[map][col][row] = new EventRect();
+                eventRect[map][col][row].x = 23;
+                eventRect[map][col][row].y = 23;
+                eventRect[map][col][row].width = 2;
+                eventRect[map][col][row].height = 2;
+                eventRect[map][col][row].eventRectDefaultX = eventRect[map][col][row].x;
+                eventRect[map][col][row].eventRectDefaultY = eventRect[map][col][row].y;
 
                 col++;
 
                 if(col == gp.maxWorldCol){
                     col = 0;
                     row++;
+
+                    if(row == gp.maxWorldRow){
+                        row = 0;
+                        map++;
+                    }
+
+
                 }
         }
 
@@ -54,50 +64,68 @@ public class EventHandler {
 
         if (canTouchEvent) {
 
-            if (hit(27, 16, "right")) {
-                damagePit(gp.dialogueState, 27, 16);
+            if (hit(0, 27, 16, "right")) {
+                damagePit(gp.dialogueState);
             }
-            if (hit(23, 42, "any")) {
+            
+            else if (hit(0, 23, 42, "any")) {
                 teleport(gp.dialogueState);
             }
-            if (hit(23, 12, "any")) {
+
+            else if (hit(0, 23, 12, "any")) {
                 healingPool(gp.dialogueState);
+            }
+
+            //Move to the next map
+            else if (hit(0, 10, 39, "any")) {
+                nextMap(1, 12, 13);
+            }
+
+            //Go back to the original map
+            else if (hit(1, 12, 13, "any")) {
+                nextMap(0, 10, 39);
             }
         }
 
     }
 
-    public boolean hit(int col, int row, String reqDirection) {
+    //TODO check this method again
+
+    public boolean hit(int map, int col, int row, String reqDirection) {
 
         boolean hit = false;
-        // PLayer's position
-        gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
-        gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
 
-        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
-        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
+        if(map == gp.currentMap){
+            // PLayer's position
+            gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
+            gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
 
-        //check if there's a collision
-        if (gp.player.solidArea.intersects(eventRect[col][row]) && !eventRect[col][row].eventDone) {
-            if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
+            eventRect[map][col][row].x = col * gp.tileSize + eventRect[map][col][row].x;
+            eventRect[map][col][row].y = row * gp.tileSize + eventRect[map][col][row].y;
 
-                hit = true;
+            //check if there's a collision
+            if (gp.player.solidArea.intersects(eventRect[map][col][row]) && !eventRect[map][col][row].eventDone) {
+                if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
 
-                previousEventX = gp.player.worldX;
-                previousEventY = gp.player.worldY;
+                    hit = true;
+
+                    previousEventX = gp.player.worldX;
+                    previousEventY = gp.player.worldY;
+                }
             }
+
+            //reset positions
+            gp.player.solidArea.x = gp.player.solidAreaDefaultX;
+            gp.player.solidArea.y = gp.player.solidAreaDefaultY;
+
+            eventRect[map][col][row].x = eventRect[map][col][row].eventRectDefaultX;
+            eventRect[map][col][row].y = eventRect[map][col][row].eventRectDefaultY;
         }
 
-        //reset positions
-        gp.player.solidArea.x = gp.player.solidAreaDefaultX;
-        gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-
-        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
-        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
         return hit;
     }
 
-    public void damagePit(int gameState, int col, int row) {
+    public void damagePit(int gameState) {
 
         gp.gameState = gameState;
         gp.playSoundEffect(10);
@@ -142,6 +170,20 @@ public class EventHandler {
 
         //TODO after one teleportation reset the tile to the original one
         //TODO MAKE THE PLAYER flicker while teleporting
+    }
+
+    public void nextMap(int map, int col, int row){
+
+        gp.currentMap = map;
+        gp.player.worldX = gp.tileSize * col;
+        gp.player.worldY = gp.tileSize * row;
+
+        previousEventX = gp.player.worldX;
+        previousEventY = gp.player.worldY;
+        canTouchEvent = false;
+
+        gp.playSoundEffect(18);
+
     }
 
 
